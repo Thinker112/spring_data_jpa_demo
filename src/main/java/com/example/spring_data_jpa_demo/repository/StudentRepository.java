7c698b0b6ef7818c51b9d00cfa3617f7f60a59be
yyb4j <17796553126@163.com>
2023-03-04 19:21:11 +0800

    (feat):delete other file

--- src/main/java/com/example/spring_data_jpa_demo/repository/StudentRepository.java
+++ src/main/java/com/example/spring_data_jpa_demo/repository/StudentRepository.java
@@ -2,10 +2,12 @@ package com.example.spring_data_jpa_demo.repository;
 
 import com.example.spring_data_jpa_demo.entity.Student;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author yyb
  * @create 2022-12-27 23:48
  */
+@Repository
 public interface StudentRepository extends CrudRepository<Student, Integer> {
 }
